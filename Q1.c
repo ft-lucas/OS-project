d944264ba3bab23084b86fc645893f7d84e3e611
ft-lucas <36597954+ft-lucas@users.noreply.github.com>
2018-04-11 01:10:11 +0530

    Update Q1.c
    average TAT can be calculated respective to process ID of processes.

--- Q1.c
+++ Q1.c
@@ -15,9 +15,34 @@ int main()
     {
         scanf("%d",&pid[i]);
     }
+    printf("Enter the BUrst Time in proper sequence: ");
     for(i=0;i<n;i++)
     {
-        printf("%d\n",&pid[i]);
+        scanf("%d",&bt[i]);
     }
-
+    wt[0]=0;
+    printf("\n\nCalulating the waiting time for every process");
+    for(i=1;i<n;i++)
+    {
+      wt[i]=0;
+      for(j=0;j<i;j++)
+      {
+       wt[i]=wt[i]+bt[j];
+      }
+    }
+    printf("\n\nCalulating the turn around time for every process");
+    for(i=0;i<n;i++)
+    {
+     tat[i]=bt[i]+wt[i];
+    }
+ int avgtat=0;
+    printf("\nProcess\tBT\tWT\tTAT");
+    for(i=0;i<n;i++)
+    {
+    printf("\nP-%d\t%d\t%d\t%d",pid[i],bt[i],wt[i],tat[i]);
+    avgwt=avgwt+wt[i];
+     avgtat=avgtat+tat[i];
+    }
+    avgtat=avgtat/n;
+    printf("\n\nAverage Turn Around Time= %d",avgtat);
 }
