92c4c11ad218133024fe2e3ab47e864a42be8895
ft-lucas <36597954+ft-lucas@users.noreply.github.com>
2018-04-12 00:49:36 +0530

    Update Q1.c
    SJF scheduling algorithm is implemented and average tat is calculated.

--- Q1.c
+++ Q1.c
@@ -1,4 +1,19 @@
- 
+   void waitingtime(int ar[],int ar2[],int n)
+    {
+    	int i,j;
+    	ar[0]=0;
+    printf("\n\nCalulating the waiting time for every process");
+    for(i=1;i<n;i++)
+    {
+      ar[i]=0;
+      for(j=0;j<i;j++)
+      {
+       ar[i]=ar[i]+ar2[j];
+      }
+    }
+    return(ar);
+	}
+
 int main()
 {
     int pid[20];
@@ -20,29 +35,63 @@ int main()
     {
         scanf("%d",&bt[i]);
     }
-    wt[0]=0;
-    printf("\n\nCalulating the waiting time for every process");
-    for(i=1;i<n;i++)
-    {
-      wt[i]=0;
-      for(j=0;j<i;j++)
-      {
-       wt[i]=wt[i]+bt[j];
-      }
-    }
+    waitingtime(wt,bt,n);
     printf("\n\nCalulating the turn around time for every process");
     for(i=0;i<n;i++)
     {
      tat[i]=bt[i]+wt[i];
     }
- int avgtat=0;
+ float avgtat=0;
     printf("\nProcess\tBT\tWT\tTAT");
     for(i=0;i<n;i++)
     {
     printf("\nP-%d\t%d\t%d\t%d",pid[i],bt[i],wt[i],tat[i]);
-    avgwt=avgwt+wt[i];
+    
      avgtat=avgtat+tat[i];
     }
     avgtat=avgtat/n;
     printf("\n\nAverage Turn Around Time= %d",avgtat);
+ 
+    printf("\n\nSolution using SHORTEST JOB FIRST: ");
+    //waitingtime(wt);
+    printf("\n Sorting the processes ");
+ int temp;   
+ for(i=0;i<n;i++)
+    {
+     for(j=0;j<n-i-1;j++)
+     {
+     if(bt[j]>bt[j+1])
+     {
+     temp=bt[j+1];
+      bt[j+1]=bt[j];
+      bt[j]=temp;
+     }
+     }
+     for(i=0;i<n;i++){
+ printf("%d",bt[i]);
+}
+}
+     for(i=0;i<n;i++){
+wt[i]=0;
+}
+
+  waitingtime(wt,bt,n);
+     for(i=0;i<n;i++){
+ printf("%d",wt[i]);
+}
+printf("\n\nCalulating the turn around time for every process");
+    for(i=0;i<n;i++)
+    {
+     tat[i]=bt[i]+wt[i];
+    }
+    avgtat=0;
+    printf("\nProcess\tBT\tWT\tTAT");
+    for(i=0;i<n;i++)
+    {
+    printf("\nP-%d\t%d\t%d\t%d",pid[i],bt[i],wt[i],tat[i]);
+    
+     avgtat=avgtat+tat[i];
+    }
+    avgtat=avgtat/n;
+    printf("\n\nAverage Turn Around Time= %f",avgtat);
 }
